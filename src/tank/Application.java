9aaddd559624d35ba9ef96945e625d44203b617d
Stephen <StephenLJosey@hotmail.com>
2017-05-08 20:45:25 -0700

    Updated title of frame for Tank Wars.

--- src/tank/Application.java
+++ src/tank/Application.java
@@ -11,7 +11,7 @@ import java.awt.event.WindowEvent;
 public class Application {
   public static void main( String[] args ) {
     final tank.GameWorld game = tank.GameWorld.getInstance();
-    JFrame frame = new JFrame( "Test" );
+    JFrame frame = new JFrame( "Tank Wars" );
     frame.addWindowListener( new WindowAdapter() {
       public void windowGainedFocus( WindowEvent e ) {
         game.requestFocusInWindow();
